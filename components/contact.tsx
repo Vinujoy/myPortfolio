73f471d2ffa526ef54b21e9e1c01b9712649e9d4
Vinu Joy <vinujoy2010@gmail.com>
2024-04-26 11:42:46 +0530

    Profolio email updated

--- components/contact.tsx
+++ components/contact.tsx
@@ -34,7 +34,7 @@ export default function Contact() {
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:vinujoy2010@gmail.com">
-          vinujoy2010@gmail.com
+          vinujoydev@gmail.com
         </a>{" "}
         or through this form.
       </p>
